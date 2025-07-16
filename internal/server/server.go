55486352a63a91dd17a31671ee614623f3884e3d
Dmitrii Palkin <dvpalkin@gmail.com>
2025-07-16 22:06:53 +0300

    s6 4st commit убрал загрузку конфига из json т.к. не проходит тест

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,8 +3,8 @@ package server
 import (
 	"log"
 	"net/http"
+	"time"
 
-	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/config"
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
 )
 
@@ -16,9 +16,9 @@ type MyServer struct {
 // инициируем сервер
 func InitServer(l *log.Logger) (*MyServer, error) {
 	//загружаем конфигурацию сервера из конфига
-	if err := config.Load("server.config"); err != nil {
+	/*if err := config.Load("server.config"); err != nil {
 		log.Fatal(err)
-	}
+	}*/
 
 	r := http.NewServeMux()
 
@@ -26,12 +26,12 @@ func InitServer(l *log.Logger) (*MyServer, error) {
 	r.HandleFunc("/upload", handlers.PostFileForm)
 
 	srv := &http.Server{
-		Addr:         config.Server.Addr,
+		Addr:         ":8080", //config.Server.Addr,
 		Handler:      r,
 		ErrorLog:     l,
-		ReadTimeout:  config.Server.ReadTimeout,
-		WriteTimeout: config.Server.WriteTimeout,
-		IdleTimeout:  config.Server.IdleTimeout,
+		ReadTimeout:  5 * time.Second,  //config.Server.ReadTimeout,
+		WriteTimeout: 10 * time.Second, //config.Server.WriteTimeout,
+		IdleTimeout:  15 * time.Second, //config.Server.IdleTimeout,
 	}
 
 	mySrv := &MyServer{
