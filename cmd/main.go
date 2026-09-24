fc1a0c3a747412bed13315ba466e9adb67f645a3
Hexo4yxaBP <63296691+Hexo4yxaBP@users.noreply.github.com>
2026-09-24 15:23:24 +0200

    Merge sprint6 API work into main (no binary)
    * s6 1st commit
    
    * s6 2nd commit добавлена обработка URL.Path в GET запросе
    
    * s6 3nd commit перенесены файлы в директорию запуска
    
    * s6 4st commit убрал загрузку конфига из json т.к. не проходит тест
    
    * 5th commit
    
    * 6st commit
    
    * 6th commit
    
    * 7th commit
    
    * 8th commit
    
    * 9th commit
    
    * Remove committed Go binary from cmd/
    
    * Ignore built binaries in cmd/
    
    ---------
    
    Co-authored-by: Dmitrii Palkin <dvpalkin@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
+
+	/*if err := os.Chdir(".."); err != nil {
+		log.Fatal(err)
+	}
+	*/
+
+	srv, err := server.InitServer(log.Default())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := http.ListenAndServe(srv.Server.Addr, srv.Server.Handler); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,44 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type MyServer struct {
+	Logger *log.Logger
+	Server *http.Server
+}
+
+// инициируем сервер
+func InitServer(l *log.Logger) (*MyServer, error) {
+	//загружаем конфигурацию сервера из конфига
+	/*if err := config.Load("server.config"); err != nil {
+		log.Fatal(err)
+	}*/
+
+	r := http.NewServeMux()
+
+	r.HandleFunc("/", handlers.GetIndexPage)
+	r.HandleFunc("/upload", handlers.PostFileForm)
+
+	srv := &http.Server{
+		Addr:         ":8080", //config.Server.Addr,
+		Handler:      r,
+		ErrorLog:     l,
+		ReadTimeout:  5 * time.Second,  //config.Server.ReadTimeout,
+		WriteTimeout: 10 * time.Second, //config.Server.WriteTimeout,
+		IdleTimeout:  15 * time.Second, //config.Server.IdleTimeout,
+	}
+
+	mySrv := &MyServer{
+		Logger: l,
+		Server: srv,
+	}
+
+	return mySrv, nil
+
+}
